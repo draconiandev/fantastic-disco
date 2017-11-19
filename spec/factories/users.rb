af35ef9407d7e560d252ba6469a7812baeb3eb9c
p150 <pavanprakash21@gmail.com>
2017-11-19 16:55:58 +0530

    Generate user model with devise and add all the extra attrs to the table

---
+++ spec/factories/users.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    email { Faker::Internet.safe_email }
+    password { Faker::Internet.password(10, 20) }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    mobile_number { [6, 7, 8, 9].sample.to_s + Faker::Number.number(9) }
+    username { Faker::Internet.user_name(Faker::Internet.user_name(4..40), '_') }
+  end
+end
